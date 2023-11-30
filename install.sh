f3a5297b73fce85ede0e07f724efa65cc88508bc
Daniel J Walsh <dwalsh@redhat.com>
2023-11-30 13:28:28 -0500

    fixes
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- install.sh
+++ install.sh
@@ -8,5 +8,6 @@ podman login quay.io
 cp $HOME/.ssh/id_rsa.pub root.keys
 podman build -t $IMAGE .
 podman push $IMAGE
+mkdir -p output
 sudo podman run --rm -it --security-opt label=type:unconfined_t --privileged -v $XDG_RUNTIME_DIR/containers/auth.json:/run/containers/0/auth.json -v ./output:/output --pull newer ghcr.io/osbuild/osbuild-deploy-container $IMAGE
 sudo chown -R $UID output
