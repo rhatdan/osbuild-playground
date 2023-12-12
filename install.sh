480d37b551f3eb185f5fb1cc492372d0205c6a1f
Daniel J Walsh <dwalsh@redhat.com>
2023-12-12 10:12:34 -0500

    Fix Containerfile to include additional store
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ if [ -z "$1" ]; then
 	exit
 fi
 IMAGE=$1
+AI_IMAGE=$2
 rootless=$(podman info --format '{{ .Host.Security.Rootless }}')
 echo $rootless
 if [[ "$rootless" = "true" ]]; then
@@ -15,12 +16,12 @@ if [[ "$rootless" = "true" ]]; then
 	    podman machine mkdir -p /tmp/output;;
     esac
 fi
-podman login quay.io
 cp $HOME/.ssh/id_rsa.pub root.keys
-podman build -t $IMAGE .
-podman push $IMAGE
-mkdir -p /tmp/output
-podman run --rm -it --security-opt label=type:unconfined_t --privileged -v /tmp/output:/output --pull newer quay.io/centos-bootc/bootc-image-builder $IMAGE
+sudo podman login quay.io
+sudo podman build --env AI_IMAGE=$AI_IMAGE --cap-add SYS_ADMIN -t $IMAGE .
+sudo podman push $IMAGE
+sudo mkdir -p /tmp/output
+sudo podman run --rm -it --security-opt label=type:unconfined_t --privileged -v /tmp/output:/output --pull newer quay.io/centos-bootc/bootc-image-builder $IMAGE
 
 case "$OSTYPE" in
 	darwin*)
